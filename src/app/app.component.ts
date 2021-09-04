74a770d1648999f9d80acaa4efbb94b6e55bde22
pamtbaau <pamtbaau@gmail.com>
2021-09-04 13:57:32 +0200

    Working Zone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
 
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Menu, MenuItemConstructorOptions, remote } from 'electron';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
 })
 export class AppComponent {
-  title = 'Zone';
+    public allHeroes: string[] = ['Hero1', 'Hero2', 'Hero3', 'Hero4'];
+    public heroes: string[] = [];
+
+    constructor() {
+        this.getHeroes();
+    }
+
+    public getHeroes() {
+        Promise.resolve().then(() => {
+            this.heroes = this.allHeroes.slice();
+        });
+    }
+
+    public restoreHeroes() {
+        this.getHeroes();
+    }
+
+    public onMenu(hero: string) {
+        const menu = this.initMenu(hero);
+        menu.popup({});
+    }
+
+    private deleteHero(hero: string) {
+        const index = this.heroes.indexOf(hero);
+        console.log(index);
+
+        Promise.resolve().then(() => {
+            this.heroes.splice(index, 1);
+            console.log(this.heroes);
+        });
+    }
+
+    private initMenu(hero: string): Menu {
+        const template: MenuItemConstructorOptions[] = [
+            {
+                label: `Delete ${hero}`,
+                click: () => this.deleteHero(hero),
+            },
+        ];
+
+        return remote.Menu.buildFromTemplate(template);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+    ],
+    declarations: [
+        AppComponent,
+    ],
+    providers: [
+    ],
+    bootstrap: [
+        AppComponent,
+    ],
 })
-export class AppModule { }
+
+export class AppModule {
+}
